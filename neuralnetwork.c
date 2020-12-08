e6ac8b90c1af6563c88886aef78853786a9ca483
arnaut-epita <55802599+arnaut-epita@users.noreply.github.com>
2020-12-08 19:23:43 +0100

    05

--- neuralnetwork.c
+++ neuralnetwork.c
@@ -10,29 +10,6 @@ int* sizesList = [8, 8, 2];
 int sizeoflist = 3;
 int sizeofInput = 2;
 
-// NeuralNetwork structure.
-typedef struct Neuron
-{
-    double net;          // Sum of weights times L-1 values + bias.
-    double val;          // Sigmoid of net.
-    double bias;         // Bias of the Neuron.
-    double* weights;     // Weights toward L-1.
-    int size;           // Size of L-1.
-} Neuron;
-
-typedef struct Layer
-{
-    Neuron* NeuronList; // List of Neuron in the Layer.
-    int size;           // Number of Neuron in the Layer.
-} Layer;
-
-typedef struct NeuralNetwork
-{
-    int* InputLayer;    // Layer that the IA Input.
-    Layer* LayerList;   // List of all Layers of the NeuralNetwork.
-    int size;           // Number of Layers not including InputLayer.
-} NeuralNetwork;
-
 // NeuralNetwork Methods.
 struct NeuralNetwork initNN()
 {
--- neuralnetwork.h
+++ neuralnetwork.h
@@ -4,9 +4,29 @@
 #ifndef NEURALNETWORK_H
 #define NEURALNETWORK_H
 
-struct Neuron;
-struct Layer;
-struct NeuralNetwork;
+// NeuralNetwork structure.
+typedef struct Neuron
+{
+    double net;          // Sum of weights times L-1 values + bias.
+    double val;          // Sigmoid of net.
+    double bias;         // Bias of the Neuron.
+    double* weights;     // Weights toward L-1.
+    int size;           // Size of L-1.
+} Neuron;
+
+typedef struct Layer
+{
+    Neuron* NeuronList; // List of Neuron in the Layer.
+    int size;           // Number of Neuron in the Layer.
+} Layer;
+
+typedef struct NeuralNetwork
+{
+    int* InputLayer;    // Layer that the IA Input.
+    Layer* LayerList;   // List of all Layers of the NeuralNetwork.
+    int size;           // Number of Layers not including InputLayer.
+} NeuralNetwork;
+
 NeuralNetwork initNN();
 
 #endif
