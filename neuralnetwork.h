09c6308df65f25197a3d41141d301436b0d9420f
arnaut-epita <55802599+arnaut-epita@users.noreply.github.com>
2020-12-08 19:19:38 +0100

    03

--- neuralnetwork.h
+++ neuralnetwork.h
@@ -4,29 +4,9 @@
 #ifndef NEURALNETWORK_H
 #define NEURALNETWORK_H
 
-// NeuralNetwork structure.
-struct Neuron
-{
-    double net;          // Sum of weights times L-1 values + bias.
-    double val;          // Sigmoid of net.
-    double bias;         // Bias of the Neuron.
-    double* weights;     // Weights toward L-1.
-    int size;           // Size of L-1.
-} Neuron;
-
-struct Layer
-{
-    Neuron* NeuronList; // List of Neuron in the Layer.
-    int size;           // Number of Neuron in the Layer.
-} Layer;
-
-struct NeuralNetwork
-{
-    int* InputLayer;    // Layer that the IA Input.
-    Layer* LayerList;   // List of all Layers of the NeuralNetwork.
-    int size;           // Number of Layers not including InputLayer.
-} NeuralNetwork;
-
+struct Neuron;
+struct Layer;
+struct NeuralNetwork;
 NeuralNetwork initNN();
 
 #endif
