7c550d6fab77418ab02a6d5285ff4f877f963f50
arnaut-epita <55802599+arnaut-epita@users.noreply.github.com>
2020-12-10 14:58:59 +0100

    no

--- src/training.c
+++ src/training.c
@@ -174,9 +174,9 @@ struct NeuralNetwork backpropagation(int* trainInput, int* trainOutput, int data
         printf("yup\n");
         //RunNeuralNetwork(nn, Input);
         printf("yup\n");
-        gn = MakeGradient(gn,nn,Output);
+        //gn = MakeGradient(gn,nn,Output);
         printf("yup\n");
-        printTry(nn,Input,Output);
+        //printTry(nn,Input,Output);
         printf("yup\n");
         nn = ApplyGradient(nn,gn);
         printf("yup\n");
